29daf252832ad95b7921a4c39bae6b0a90a119a1
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2024-08-10 08:56:48 +0600

    .

--- DSA_ALGO_GRIND75/Stack_Queue/155.py
+++ DSA_ALGO_GRIND75/Stack_Queue/155.py
@@ -36,7 +36,7 @@ class MinStack:
         self.stack = []
         self.minn = float("inf")
 
-    def push(self, val: int) -> None:
+    def push(self, val: int) -> None: 
         if val <= self.minn:
             self.stack.append(self.minn)
             self.minn = val
@@ -66,3 +66,34 @@ if __name__ == "__main__":
     param_4 = obj.getMin()
     print(param_3)
     print(param_4)
+
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+        self.min = float('inf')
+        self.min_stack = [self.min]
+
+    def push(self, val: int) -> None:
+        if val <= self.min:
+            self.min_stack.append(val)
+            self.min = val
+        else:
+            self.min_stack.append(self.min)
+
+        self.stack.append(val)
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+        self.min = self.min_stack[-1]
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
--- DSA_ALGO_GRIND75/String/76.py
+++ DSA_ALGO_GRIND75/String/76.py
@@ -38,3 +38,40 @@ class Solution:
 
         l, r = result
         return s[l:r+1] if result_length != float("infinity") else ""
+
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t) or len(s) == 0 or t == "":
+            return ""
+
+        t_counter = Counter(t)
+        temp = Counter()
+        left, right = 0, 0
+
+        result, result_length = [-1, -1], float("infinity")
+
+        while right < len(s):
+            if s[right] in t_counter:
+                temp[s[right]] += 1
+
+            if temp == t_counter: #or
+                while left <= right and temp == t_counter:
+                    if (right - left) < result_length:
+                        result = [left, right]
+                        result_length = (right - left)
+
+                    if s[left] in t_counter:
+                        temp[s[left]] -= 1
+
+                    left += 1
+
+            right += 1
+
+        if result[0] == -1:
+            return ""
+        
+        l, r = result
+
+        return s[l:r+1] if result_length != float("infinity") else ""
