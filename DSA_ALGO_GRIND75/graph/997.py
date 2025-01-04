61c0a8f1eeb4a1376e1774e387cbdaa269849246
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-01-04 10:10:11 +0600

    .

---
+++ DSA_ALGO_GRIND75/graph/997.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        adj_list = defaultdict(list)
+
+        for a,b in trust:
+            adj_list[a].append(b)
+
+        for i in range(1,n+1):
+            if len(adj_list[i]) == 0:
+                judge = i
+                break
+            else:
+                judge = -1
+
+        if judge == -1:
+            return -1
+        
+        for i in range(1,n+1):
+            if i == judge:
+                continue
+            if judge not in adj_list[i]:
+                return -1
+            
+        return judge
