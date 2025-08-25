a3260a6e724a5b7630b1ba2c16ac3e32d44c01a5
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-08-25 11:10:16 +0600

    Implement LinkedList class and Node class; add methods for appending and printing list elements

--- data_structure/linked_list.py
+++ data_structure/linked_list.py
@@ -1,16 +1,31 @@
-class Node:
+from node import Node
+
+
+class LinkedList:
     def __init__(self,val):
-        self.val = val
-        self.next = None
+        new_node = Node(val)
+        self.head = new_node
+        self.tail = new_node
+        self.len = 1
+ 
+    def append(self,val):
+        new_node = Node(val)
+        self.tail.next = new_node
+        self.tail = new_node
+        self.len += 1
 
-a = Node(2)
-b = Node(3)
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.val)
+            temp = temp.next
 
-a.next = b 
-c = Node(4)
+llist = LinkedList(1)
+llist.append(2) 
+llist.append(3)
+llist.append(4) 
 
-b.next = c
-
-while a:
-    print(a.val)
-    a = a.next
+llist.print_list()
+a = 2
+print(type(a))
+print(__name__)
