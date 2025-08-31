61a72929a459e2a71ad8949faac87fd3db0dda0b
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-09-01 00:47:43 +0600

    Refactor LinkedList class: add index, insert, pop, and remove methods; improve functionality and maintainability.

--- data_structure/linked_list.py
+++ data_structure/linked_list.py
@@ -14,6 +14,66 @@ class LinkedList:
         self.tail = new_node
         self.len += 1
 
+    def index(self, ind):
+        if ind < 0  or ind >= self.len:
+            raise IndexError('Index out of bound')
+        
+        temp = self.head
+
+        for i in range(ind):
+            temp = temp.next
+
+        return temp.val
+    
+
+    def insert(self, ind, val):
+        prev = self.head
+
+        for i in range(ind-1):
+            prev = prev.next
+
+        next = prev.next
+
+        new_node = Node(val)
+
+        prev.next = new_node
+        new_node.next = next
+        self.len += 1
+
+    def pop(self):
+        prev = self.head
+
+        while prev.next.next:
+            prev = prev.next
+
+        prev.next = None 
+
+        temp = self.tail
+
+        self.tail = prev
+        self.len -= 1
+
+        return temp.val
+    
+
+    def remove(self, ind):
+        prev = self.head
+        
+        for i in range(ind-1):
+            prev = prev.next
+
+        curr = prev.next
+        next = curr.next
+
+        prev.next = next
+        curr.next = None
+
+        self.len -= 1
+
+        return curr.val
+        
+
+
     def print_list(self):
         temp = self.head
         while temp:
@@ -26,6 +86,13 @@ llist.append(3)
 llist.append(4) 
 
 llist.print_list()
-a = 2
-print(type(a))
-print(__name__)
+print(f'Index val: {llist.index(0)}') 
+
+llist.insert(2,6)
+
+llist.print_list()
+
+val = llist.remove(1)
+
+print(f'Removed val: {val}')
+llist.print_list()
