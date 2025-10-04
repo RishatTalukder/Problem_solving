e5d1b72a84405bbba16e46b9e1e62a456593cf84
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-10-04 11:34:36 +0600

    Implement maxArea method in Solution class: add two-pointer approach to calculate maximum area between bars in a histogram.

--- DSA_ALGO_GRIND75/ARRAY/11.py
+++ DSA_ALGO_GRIND75/ARRAY/11.py
@@ -47,3 +47,24 @@ class Solution:
 
 a = [1,1]
 print(Solution().maxArea(a))
+
+
+class Solution:
+    def maxArea(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums)-1
+
+        ans = 0
+
+        while left < right:
+            area = min(nums[left], nums[right])*(right - left)
+            ans = max(ans, area)
+
+            if nums[left] < nums[right]:
+                left += 1
+
+            else:
+                right -= 1
+
+
+        return ans
