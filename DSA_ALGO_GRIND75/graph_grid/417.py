b45c54a5c1656f77936461667ee8d49a6cadd8de
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-10-05 13:01:40 +0600

    Fix variable name in pacificAtlantic method and implement BST class with insert and inorder print functionality

--- DSA_ALGO_GRIND75/graph_grid/417.py
+++ DSA_ALGO_GRIND75/graph_grid/417.py
@@ -1,6 +1,6 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not matrix:
+        if not heights:
             return []
 
         pacific = set()
@@ -30,3 +30,40 @@ class Solution:
 
 
         return list(pacific & atlantic)
+    
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights:
+            return []
+        
+        pecific = set()
+        atlantic = set()
+
+        rows = len(heights)
+        cols = len(heights[0])
+
+        def dfs(r,c,ocean:set):
+            ocean.add((r,c))
+            directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+            for dr,dc in directions:
+                nr, nc = r+dr, c+dc
+                if (
+                    0 <= nr < rows and
+                    0 <= nc < cols and
+                    (nr,nc) not in ocean and
+                    heights[nr][nc] >= heights[r][c]
+                ):
+                    dfs(nr,nc,ocean)
+
+        for i in range(rows):
+            dfs(i,0,pecific)
+            dfs(i,cols-1,atlantic)
+
+        for j in range(cols):
+            dfs(0,j,pecific)
+            dfs(rows-1,j,atlantic)
+
+        return list(pecific & atlantic)
+        
