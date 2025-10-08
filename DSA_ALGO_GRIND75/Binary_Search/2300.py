141f3708a9433e0b9bf8d1e0ba4e0ec410862ddc
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2025-10-08 11:18:00 +0600

    Implement successfulPairs method in Solution class: calculate number of successful pairs of spells and potions based on success criteria.

---
+++ DSA_ALGO_GRIND75/Binary_Search/2300.py
@@ -0,0 +1,17 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        c_potion = len(potions)
+        result = []
+
+        for spell in spells:
+            required = math.ceil(success / spell)
+
+            if required > potions[-1]:
+                result.append(0)
+                continue
+
+            ind = bisect.bisect_left(potions, required)
+            result.append(c_potion - ind)
+
+        return result
