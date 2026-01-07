46d032a3554b70e3858f1c79776b74319d4ee86a
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2026-01-07 12:43:16 +0600

    Implement maxProduct method in Solution class to calculate the maximum product of split binary tree; add TreeNode definition

--- Leetcode_Solutions/Binary_Tree/bfs/1161.py
+++ Leetcode_Solutions/Binary_Tree/bfs/1161.py
@@ -36,4 +36,44 @@ class Solution:
         return ans
             
 
-            
+
+from collections import deque
+from typing import Optional
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+
+        current_level = 1
+        max_sum = root.val
+        max_sum_level = 1
+
+        while queue:
+            next_queue = deque()
+            level_sum = 0
+
+            for node in queue:
+                if node.left:
+                    level_sum += node.left.val
+                    next_queue.append(node.left)
+
+                if node.right:
+                    level_sum += node.right.val
+                    next_queue.append(node.right)
+
+            current_level += 1
+
+            if next_queue and level_sum > max_sum:
+                max_sum = level_sum
+                max_sum_level = current_level
+
+            queue = next_queue
+
+        return max_sum_level
