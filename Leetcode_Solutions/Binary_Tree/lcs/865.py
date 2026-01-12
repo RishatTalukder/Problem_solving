9608470b4600e27a3156590fc0034b26722fea85
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2026-01-12 11:44:33 +0600

    Implement subtreeWithAllDeepest method in Solution class to find the subtree containing all the deepest nodes of a binary tree; add TreeNode definition Implement minimumDeleteSum method in Solution class to calculate the minimum ASCII delete sum for two strings Add minTimeToVisitAllPoints method in Solution class to calculate the minimum time to visit all points in a 2D plane

--- Leetcode_Solutions/Binary_Tree/lcs/865.py
+++ Leetcode_Solutions/Binary_Tree/lcs/865.py
@@ -83,5 +83,41 @@ class Solution:
         return deep.pop()
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        parents = {root:None}
+
+        q = deque([root])
+        last = []
+        while q:
+            last = []
+            n = len(q)
+
+            for i in  range(n):
+                node = q.popleft()
+                last.append(node)
+
+                if node.left:
+                    parents[node.left] = node
+                    q.append(node.left)
+
+                if node.right:
+                    parents[node.right] = node
+                    q.append(node.right)
 
 
+        deep = set(last)
+
+        while len(deep) > 1:
+            deep = {parents[node] for node in deep}
+
+        return deep.pop()
--- Leetcode_Solutions/dp/lcs/712.py
+++ Leetcode_Solutions/dp/lcs/712.py
@@ -15,3 +15,25 @@ class Solution:
         total = sum((ord(c) for c in s1)) + sum((ord(c) for c in s2))
 
         return total - 2*dp[m][n]
+    
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m = len(s1)
+        n = len(s2)
+
+        dp = [[0]*(n+1) for _ in  range(m+1)]
+
+        for i in range(m):
+            for j in range(n):
+                if s1[i] == s2[j]:
+                    dp[i+1][j+1] = dp[i][j] + ord(s1[i])
+
+                else:
+                    dp[i+1][j+1] = max(
+                        dp[i+1][j],
+                        dp[i][j+1]
+                    )
+
+        total = sum(ord(c) for c in s1) + sum(ord(c) for c in s2)
+
+        return total - 2*dp[m][n] 
