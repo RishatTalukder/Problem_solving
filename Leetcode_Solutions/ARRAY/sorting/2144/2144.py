ae88e9335051701135d1a8468940c212759bfef1
Md. Rishat Talukder <120198784+RishatTalukder@users.noreply.github.com>
2026-06-01 11:02:50 +0600

    .

---
+++ Leetcode_Solutions/ARRAY/sorting/2144/2144.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        cost.sort(reverse=True)
+        summ = sum(cost)
+
+        for i in range(2, len(cost), 3):
+            summ -= cost[i]
+
+        return summ
